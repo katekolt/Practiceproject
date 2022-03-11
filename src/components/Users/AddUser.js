37489cebda98fbeee9174f45de11b3a97be37efb
Kateryna Koltovska <79753036+katekolt@users.noreply.github.com>
2022-03-11 20:40:46 +0200

    first commit ( re- usable Button)

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,20 @@
+import classes from './AddUser.module.css';
+import Card from "../UI/Card";
+import Button from '../UI/Button';
+
+
+const AddUser = (props)  => {
+    const addUserHandler = (event) => {
+        event.preventDefault();
+    };
+    return (
+        <Card className={classes.input} onSubmit={addUserHandler}>
+            <label htmlFor='username'>Username</label>
+            <input id='username' type='text'/>
+            <label htmlFor='age'>Age (Years)</label>
+            <input id='age' type='number'/>
+            <Button type='submit'>Add User</Button>
+        </Card>
+    )
+}
+export default AddUser;
